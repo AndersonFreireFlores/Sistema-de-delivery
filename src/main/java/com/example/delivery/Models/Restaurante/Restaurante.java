cfacb300d3627d759a1d1a576e3216da47a3cb15
ander <andersonfflores75@gmail.com>
2024-11-13 14:56:35 -0300

    Restaurante entity

---
+++ src/main/java/com/example/delivery/Models/Restaurante/Restaurante.java
@@ -0,0 +1,64 @@
+package com.example.delivery.Models.Restaurante;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+@Entity
+public class Restaurante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String nome;
+
+    private String endereco;
+
+    private String telefone;
+
+    public Restaurante() {
+    }
+
+    public Restaurante(UUID id, String nome, String endereco, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
